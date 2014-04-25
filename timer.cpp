f2374971db47408828bbc675d94db84b17a7eb23
slylinux <sun_linux@126.com>
2014-04-25 14:48:30 +0800

    timer

--- timer.cpp
+++ timer.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 void print(const boost::system::error_code&,boost::asio::deadline_timer* t,int* count)
 {
-  std::cout<<"timer!"<<std::endl;
   t->expires_at(t->expires_at()+boost::posix_time::seconds(2));
   t->async_wait(boost::bind(print,boost::asio::placeholders::error,t,count));
 }
